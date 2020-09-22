0402acae05def350f5029128a3f122c9d9e0dac3
AdwinKing <jin2000112014@gmail.com>
2020-09-22 19:28:01 +0800

    added a 2d demo

--- well_1d.py
+++ well_1d.py
@@ -11,20 +11,23 @@ import time
 # free line [0, 1].
 # initial wave function is a 1d Gaussian wave packet
 free_line = (0, 1)
-grid_size = 100
-packet_width = 0.05
-direction_vector = -1
-time_step = 0.01
-num_of_frames = 10000
+grid_size = 10
+packet_width = 0.1
+direction_vector = 10000
+time_step = 0.0001
+num_of_frames = 100
+num_of_partitions_H = 800
 
 free_line_length = free_line[1] - free_line[0]
 mesh_step = free_line_length / grid_size
+mesh_step_reciprocal = grid_size / free_line_length
 
 def initial_wave_unnormalized(x):
     # with width 0.01 and direction vector k=1
     if x < free_line[0] or x > free_line[1]:
         return 0
-    return cmath.exp(-x**2/4/packet_width**2 + x*direction_vector*1j)
+    return cmath.exp(-(x-0.5)**2/4/packet_width**2 + x*direction_vector*1j)
+    # return cmath.exp(-(x-0.5)**2/4/packet_width**2)
 
 def get_normalization_factor():
     integral = 0
@@ -46,49 +49,63 @@ def get_discretized_init_wave_function():
     # print([abs(x)**2 for x in list(results)])
     return results
 
+# the operator is not unitary.
 def get_evolution_operator(t):
-    exph0 = np.identity(grid_size) * t / mesh_step**2 * (-2j)
-    exph0 = scipy.linalg.expm(exph0)
+    exph0 = np.identity(grid_size) * 2
+    for i in range(grid_size - 1):
+        exph0[i][i + 1] = -1
+        exph0[i + 1][i] = -1
+    evolution_operator = scipy.linalg.expm(exph0 * t *(-1j) * mesh_step_reciprocal**2 / num_of_partitions_H)**num_of_partitions_H
+    print(np.linalg.det(evolution_operator * scipy.linalg.expm(exph0 * t * 1j * mesh_step_reciprocal**2)))
 
-    exph1 = np.zeros((grid_size, grid_size));
-    for i in range(0, grid_size - 1, 2):
-        exph1[i][i + 1] = 1
-        exph1[i + 1][i] = 1
-    exph1 = exph1 * 1j * t / mesh_step**2
-    exph1 = scipy.linalg.expm(exph1)
+    # exph0 = np.identity(grid_size) * (-2j) * t * mesh_step_reciprocal**2
+    # exph0 = scipy.linalg.expm(exph0)
+    #
+    # exph1 = np.zeros((grid_size, grid_size), dtype=np.complex128);
+    # temp2d = np.array([[0, 1], [1, 0]], dtype=np.complex128) * (-1j) * t * mesh_step_reciprocal**2
+    # temp2d = scipy.linalg.expm(temp2d)
+    # for i in range(0, grid_size - 1, 2):
+    #     exph1[np.ix_([i, i + 1], [i, i + 1])] = temp2d
+    #
+    # # zeros here
+    # exph2 = np.zeros((grid_size, grid_size), dtype=np.complex128);
+    # for i in range(1, grid_size - 2, 2):
+    #     exph2[np.ix_([i, i + 1], [i, i + 1])] = temp2d
+    # exph2[0][0] = 1
+    # exph2[grid_size - 1][grid_size - 1] = 1
+    #
+    # evolution_operator = exph0 * exph1 * exph2
+    # print(np.linalg.det(evolution_operator * evolution_operator.conj().transpose()))
 
-    exph2 = np.zeros((grid_size, grid_size));
-    for i in range(1, grid_size - 1, 2):
-        exph2[i][i + 1] = 1
-        exph2[i + 1][i] = 1
-    exph2 = exph2 * 1j * t / mesh_step**2
-    exph2 = scipy.linalg.expm(exph2)
-
-    return exph0 * exph1 * exph2
+    return evolution_operator
 
 def get_discretized_wave_function(t):
     return get_evolution_operator(t).dot(get_discretized_init_wave_function())
 
 def get_probability_distribution(t):
     ys = [abs(x)**2 for x in list(get_discretized_wave_function(t))]
+    # print(sum(ys))
     xs = np.linspace(0, 1, grid_size)
     return (xs, ys)
 
+for i in range(num_of_frames):
+    get_probability_distribution(i * time_step)
+
 # draw the figure
-fig, ax = plt.subplots()
-xdata, ydata = [], []
-line, = ax.plot([], [], 'o')
-
-def init():
-    ax.set_xlim(free_line[0], free_line[1])
-    ax.set_ylim(0, 0.5)
-    return line,
-
-def update(frame):
-    dis = get_probability_distribution(frame)
-    line.set_data(dis[0], dis[1])
-    return line,
-
-ani = FuncAnimation(fig, update, frames=np.linspace(0, num_of_frames * time_step, num_of_frames),
-                    init_func=init, blit=True)
-plt.show()
+# fig, ax = plt.subplots()
+# xdata, ydata = [], []
+# line, = ax.plot([], [], 'o')
+#
+# def init():
+#     ax.set_xlim(free_line[0], free_line[1])
+#     ax.set_ylim(0, 5)
+#     return line,
+#
+# def update(frame):
+#     dis = get_probability_distribution(frame * time_step)
+#     line.set_data(dis[0], dis[1])
+#     return line,
+#
+# ani = FuncAnimation(fig, update, frames=num_of_frames,
+#                     init_func=init, blit=True, repeat=False, interval=0.0001)
+# plt.show()
---
+++ well_2d.py
@@ -0,0 +1,97 @@
+import numpy as np
+import scipy.special
+import cmath
+import math
+
+
+
+free_plane = (0, 0, 1, 1)
+grid_size = (20, 20)
+packet_width_x = 0.1
+packet_width_y = 0.2
+direction_vector = 10000
+time_step = 0.0001
+num_of_frames = 100
+num_of_partitions_H = 800
+order_of_chebyshev_poly = 32
+
+free_plane_length = (free_plane[2] - free_plane[0], free_plane[3] - free_plane[1])
+mesh_step = free_plane_length[0] / grid_size[0]
+mesh_step_reciprocal = grid_size[0] / free_plane_length[0]
+operator_size = grid_size[0] * grid_size[1]
+
+def initial_wave_unnormalized(x, y):
+    # with width 0.01 and direction vector k=1
+    if x < free_plane[0] or x > free_plane[2] or y < free_plane[1] or y > free_plane[3]:
+        return 0
+    return cmath.exp(-(x-0.5)**2/4/packet_width_x**2 -(y - 0.5)**2/4/packet_width_y**2 + x*direction_vector*1j)
+
+def get_normalization_factor():
+    integral = 0
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            integral += abs(initial_wave_unnormalized(i * mesh_step, j * mesh_step))**2
+    integral *= mesh_step
+    return math.sqrt(1/integral)
+
+normalization_factor = get_normalization_factor()
+def initial_wave_normalized(x, y):
+    return initial_wave_unnormalized(x, y) * normalization_factor
+
+
+def get_discretized_init_wave_function():
+    results = []
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            results.append(initial_wave_normalized(i * mesh_step, j * mesh_step))
+    # results = np.transpose(np.array(results))
+    # print([abs(x)**2 for x in list(results)])
+    return results
+
+def flatten_hamiltionian(i, j):
+    rowH = np.zeros((grid_size[0], grid_size[1]))
+    rowH[i][j] = -4
+    if i + 1 < grid_size[0]:
+        rowH[i + 1][j] = 1
+    if i - 1 >= 0:
+        rowH[i - 1][j] = 1
+    if j + 1 < grid_size[1]:
+        rowH[i][j + 1] = 1
+    if j - 1 >= 0:
+        rowH[i][j - 1] = 1
+    return rowH.flatten()
+
+
+def get_hamiltonian():
+    hamiltonian = []
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            hamiltonian.append(flatten_hamiltionian(i, j))
+    return np.array(hamiltonian)
+
+T_tilde_matrices = {}
+def T_tilde_matrix(order, B):
+    if order in T_tilde_matrices:
+        return T_tilde_matrices[order]
+    result = None
+    if order == 0:
+        result = np.identity(operator_size)
+    elif order == 1:
+        result = B * 1j
+    else:
+        result = B * 2j * T_tilde_matrix(order - 1, B) + T_tilde_matrix(order - 2, B)
+    T_tilde_matrices.update({order:result})
+    return result
+
+def get_evolution_operator(t):
+    H = get_hamiltonian()
+    max_entry = np.amax(H) * 16
+    z = -t * max_entry
+    B = H / max_entry
+    evolution_operator = np.identity(operator_size) * scipy.special.jv(0, z) + 2 * sum([scipy.special.jv(i, z) * T_tilde_matrix(i, B) for i in range(1, order_of_chebyshev_poly)])
+    return evolution_operator
+
+def verify_evolution_operator(U):
+    print(np.linalg.det(U * U.conj().transpose()))
+
+verify_evolution_operator(get_evolution_operator(time_step * 10000))
