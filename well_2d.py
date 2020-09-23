28d1d2fe389839daaa47a1850c029a8167718eaf
AdwinKing <jin2000112014@gmail.com>
2020-09-23 15:08:26 +0800

    use dynamic order to control the precision

--- well_2d.py
+++ well_2d.py
@@ -12,16 +12,17 @@ free_plane = (0, 0, 1, 1)
 grid_size = (20, 20)
 packet_width_x = 0.1
 packet_width_y = 0.2
-direction_vector = 10000
-time_step = 0.1
+direction_vector = 10
+time_step = 0.001
 num_of_frames = 100
-num_of_partitions_H = 800
-order_of_chebyshev_poly = 64
+max_order_of_chebyshev_poly = 1000000
+allowed_error = 10**(-13)
 
 free_plane_length = (free_plane[2] - free_plane[0], free_plane[3] - free_plane[1])
 mesh_step = free_plane_length[0] / grid_size[0]
 mesh_step_reciprocal = grid_size[0] / free_plane_length[0]
 operator_size = grid_size[0] * grid_size[1]
+
 
 def initial_wave_unnormalized(x, y):
     # with width 0.01 and direction vector k=1
@@ -47,9 +48,10 @@ def get_discretized_init_wave_function():
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
             results.append(initial_wave_normalized(i * mesh_step, j * mesh_step))
-    # results = np.transpose(np.array(results))
-    # print([abs(x)**2 for x in list(results)])
     return np.array(results)
+
+def get_potential(x, y):
+    return x * 10000 + y * 10000
 
 def flatten_hamiltionian(i, j):
     rowH = np.zeros((grid_size[0], grid_size[1]))
@@ -62,6 +64,8 @@ def flatten_hamiltionian(i, j):
         rowH[i][j + 1] = 1
     if j - 1 >= 0:
         rowH[i][j - 1] = 1
+    rowH = rowH * mesh_step_reciprocal**2
+    rowH[i][j] += get_potential(i * mesh_step, j * mesh_step)
     return rowH.flatten()
 
 
@@ -86,16 +90,28 @@ def T_tilde_matrix(order, B):
     T_tilde_matrices.update({order:result})
     return result
 
+H = get_hamiltonian()
+max_entry = np.amax(np.abs(H))
 def get_evolution_operator(t):
-    H = get_hamiltonian()
-    max_entry = np.amax(H) * 16
     z = -t * max_entry
     B = H / max_entry
-    evolution_operator = np.identity(operator_size) * scipy.special.jv(0, z) + 2 * sum([scipy.special.jv(i, z) * T_tilde_matrix(i, B) for i in range(1, order_of_chebyshev_poly)])
+    # evolution_operator = np.identity(operator_size) * scipy.special.jv(0, z) + 2 * sum([scipy.special.jv(i, z) * T_tilde_matrix(i, B) for i in range(1, order_of_chebyshev_poly)])
+    evolution_operator = np.zeros((operator_size, operator_size), dtype=np.complex128)
+    jv = 1
+    i = 1
+    while abs(jv) > allowed_error and i <= max_order_of_chebyshev_poly:
+        jv = scipy.special.jv(i, z)
+        evolution_operator += jv * T_tilde_matrix(i, B)
+        i += 1
+    evolution_operator = evolution_operator * 2 + np.identity(operator_size, dtype=np.complex128) * scipy.special.jv(0, z)
+    print("{} : {}".format(i, abs(jv)))
     return evolution_operator
 
 def verify_evolution_operator(U):
-    print(np.linalg.det(U * U.conj().transpose()))
+    # print(np.sum(U))
+    det = np.linalg.det(U * U.conj().transpose())
+    # print(det)
+    return det
 
 def get_wave_distribution(t):
     U = get_evolution_operator(t)
@@ -105,6 +121,14 @@ def get_wave_distribution(t):
 def get_probability_distribution(t):
     W = get_wave_distribution(t)
     return np.abs(W)**2
+
+
+
+# for i in range(0, 1000000, 16):
+#     res = verify_evolution_operator(get_evolution_operator(time_step * i))
+#     print("{} : {}".format(i, res))
+
+
 
 
 xs = np.linspace(free_plane[0], free_plane[2], grid_size[0])
@@ -117,8 +141,9 @@ def update_plot(frame_number):
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+ax.set_zlim(0, 1)
+ax.invert_xaxis()
 
-# plot = [ax.plot_surface(xs, ys, get_probability_distribution(0), color='0.75', rstride=1, cstride=1)]
 ax.plot_surface(xs, ys, get_probability_distribution(0), color='0.75', rstride=1, cstride=1, cmap="coolwarm")
 ani = FuncAnimation(fig, update_plot, num_of_frames, interval=1000/60)
 
