92d2ef19f957c42d7a1d54df0d8da21295f35007
AdwinKing <jin2000112014@gmail.com>
2020-09-26 11:10:46 +0800

    use sparse matrices instead of dense ones

--- PropagateGaussianWavePacket.cpp
+++ PropagateGaussianWavePacket.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iostream>
 #include <chrono>
+#include <omp.h>
+#include <algorithm>
 
 using namespace Eigen;
 using namespace std;
@@ -51,32 +53,45 @@ double getPotential(const double &x, const double &y) {
     return 0.;
 }
 
-VectorXd flattenedRowHamiltionian(const long &i, const long &j) {
-    VectorXd row(OPERATOR_SIZE);
-    row = VectorXd::Constant(OPERATOR_SIZE, 0.0);
-    row(j * GRID_SIZE[1] + i) = -4;
-    if (i + 1 < GRID_SIZE[0]) {
-        row(j * GRID_SIZE[1] + i + 1) = 1;
-    }
-    if (i - 1 >= 0) {
-        row(j * GRID_SIZE[1] + i - 1) = 1;
-    }
-    if (j + 1 < GRID_SIZE[1]) {
-        row((j + 1) * GRID_SIZE[1] + i) = 1;
-    }
-    if (j - 1 >= 0) {
-        row((j - 1) * GRID_SIZE[1] + i) = 1;
-    }
-    row = row / pow(MESH_STEP, 2);
-    row((j * GRID_SIZE[1] + i)) += getPotential(i * MESH_STEP, j * MESH_STEP);
-    return row;
-}
+//SparseVector<complex<double>> flattenedRowHamiltionian(const long &i, const long &j) {
+//    SparseVector<complex<double>> row(OPERATOR_SIZE);
+//    row = VectorXd::Constant(OPERATOR_SIZE, 0.0);
+//    row.insert(j * GRID_SIZE[1] + i) = -4;
+//    if (i + 1 < GRID_SIZE[0]) {
+//        row.insert(j * GRID_SIZE[1] + i + 1) = 1;
+//    }
+//    if (i - 1 >= 0) {
+//        row.insert(j * GRID_SIZE[1] + i - 1) = 1;
+//    }
+//    if (j + 1 < GRID_SIZE[1]) {
+//        row.insert((j + 1) * GRID_SIZE[1] + i) = 1;
+//    }
+//    if (j - 1 >= 0) {
+//        row.insert((j - 1) * GRID_SIZE[1] + i) = 1;
+//    }
+//    row = row / pow(MESH_STEP, 2);
+//    row(j * GRID_SIZE[1] + i) += getPotential(i * MESH_STEP, j * MESH_STEP);
+//    return row;
+//}
 
-MatrixXcd getHamiltonian() {
-    MatrixXcd H(OPERATOR_SIZE, OPERATOR_SIZE);
-    for (size_t j = 0; j < GRID_SIZE[1]; ++j) {
-        for (size_t i = 0; i < GRID_SIZE[0]; ++i) {
-            H.block<1, OPERATOR_SIZE>(j * GRID_SIZE[1] + i, 0) = flattenedRowHamiltionian(i, j).transpose();
+SparseMatrix<complex<double>> getHamiltonian() {
+    SparseMatrix<complex<double>> H(OPERATOR_SIZE, OPERATOR_SIZE);
+    for (long j = 0; j < GRID_SIZE[1]; ++j) {
+        for (long i = 0; i < GRID_SIZE[0]; ++i) {
+            //H.block<1, OPERATOR_SIZE>(j * GRID_SIZE[1] + i, 0) = flattenedRowHamiltionian(i, j).transpose();
+            H.insert(j * GRID_SIZE[1] + i, j * GRID_SIZE[1] + i) =  -4. / pow(MESH_STEP, 2) + getPotential(i * MESH_STEP, j * MESH_STEP);
+            if (i + 1 < GRID_SIZE[0]) {
+                H.insert(j * GRID_SIZE[1] + i, j * GRID_SIZE[1] + i + 1) = 1. / pow(MESH_STEP, 2);
+            }
+            if (i - 1 >= 0) {
+                H.insert(j * GRID_SIZE[1] + i, j * GRID_SIZE[1] + i - 1) = 1. / pow(MESH_STEP, 2);
+            }
+            if (j + 1 < GRID_SIZE[1]) {
+                H.insert(j * GRID_SIZE[1] + i, (j + 1) * GRID_SIZE[1] + i) = 1. / pow(MESH_STEP, 2);
+            }
+            if (j - 1 >= 0) {
+                H.insert(j * GRID_SIZE[1] + i, (j - 1) * GRID_SIZE[1] + i) = 1. / pow(MESH_STEP, 2);
+            }
         }
     }
     return H;
@@ -98,41 +113,91 @@ size_t findProperApproximationOrder(double z) {
 }
 
 
-const auto H = getHamiltonian();
-const auto maxEntry = H.cwiseAbs().maxCoeff();
-MatrixXcd getEvolutionOperatorForOneTimestep() {
-    auto begin = chrono::system_clock::now();
+SparseMatrix<complex<double>> getTildeTSparseMatrix(const size_t &order, const SparseMatrix<complex<double>> &B, vector<SparseMatrix<complex<double>>> &tildeTMatrices) {
+    if (order < tildeTMatrices.size()) {
+        return tildeTMatrices[order];
+    }
+    if (order == 0) {
+        SparseMatrix<complex<double>> mat;
+        mat.setIdentity();
+        tildeTMatrices.push_back(mat);
+        return mat;
+    } else if (order == 1) {
+        auto mat = B * complex<double>(0, 1);
+        tildeTMatrices.push_back(mat);
+        return mat;
+    } else {
+        auto mat = B * complex<double>(0, 2) * getTildeTSparseMatrix(order - 1, B, tildeTMatrices) + getTildeTSparseMatrix(order - 2, B, tildeTMatrices);
+        tildeTMatrices.push_back(mat);
+        return mat;
+    }
+}
+
+
+
+
+SparseMatrix<complex<double>> getEvolutionOperatorForOneTimestep() {
+    omp_set_num_threads(omp_get_max_threads());
+    #pragma omp declare reduction(matrixAdd : SparseMatrix<complex<double>> : omp_out = omp_out + omp_in)                             \
+
+
+    const auto H = getHamiltonian();
+    //const auto maxEntry = H.cwiseAbs().maxCoeff();
+    const auto pointerOfH = H.valuePtr();
+    vector<complex<double>> valuesOfH(pointerOfH, pointerOfH + H.nonZeros());
+    transform(valuesOfH.begin(), valuesOfH.end(), valuesOfH.begin(), abs<complex<double>>);
+    vector<double> normValues(valuesOfH.size());
+    for (size_t i = 0; i < valuesOfH.size(); ++i) {
+        normValues[i] = valuesOfH[i].real();
+    }
+    const double maxEntry = *max_element(normValues.begin(), normValues.end());
     double z = TIME_STEP * maxEntry;
     auto B = -H / maxEntry;
-    MatrixXcd evolutionOperator;
-    evolutionOperator = MatrixXcd::Zero(OPERATOR_SIZE, OPERATOR_SIZE);
+    SparseMatrix<complex<double>> evolutionOperator;
+    evolutionOperator.setZero();
     complex<double> jv = 1;
-    vector<MatrixXcd> tildeTMatrices = {MatrixXcd::Identity(OPERATOR_SIZE, OPERATOR_SIZE), B * complex<double>(0., 1.)};
     auto properOrder = findProperApproximationOrder(z);
+    cout << properOrder << endl;
+    vector<SparseMatrix<complex<double>>> tildeTMatrices(properOrder + 1);
+    for (size_t i = 0; i <= properOrder; ++i) {
+        getTildeTSparseMatrix(i, B, tildeTMatrices);
+    }
+    #pragma omp parallel for reduction(matrixAdd : evolutionOperator)
     for (size_t i = 1; i <= properOrder; ++i) {
         jv = cyl_bessel_j(i, z);
-        size_t count = 0;
-        for (size_t j = 0; j < OPERATOR_SIZE; ++j) {
-            for (size_t k = 0; k < OPERATOR_SIZE; ++k) {
-                if (tildeTMatrices[0](j, k) == 0.0) {
-                    count++;
-                }
-            }
-        }
-        cout << OPERATOR_SIZE * OPERATOR_SIZE - count << endl;
-        evolutionOperator += jv * tildeTMatrices[0];
-        auto nextTildeT = B * complex<double>(0., 2.) * tildeTMatrices[1] + tildeTMatrices[0];
-        tildeTMatrices[0] = tildeTMatrices[1];
-        tildeTMatrices[1] = nextTildeT;
+        evolutionOperator = jv * tildeTMatrices[i];
         cout << i << endl;
     }
     evolutionOperator *= 2.0;
-    evolutionOperator += MatrixXcd::Identity(OPERATOR_SIZE, OPERATOR_SIZE) * cyl_bessel_j(0, z);
-    auto end = chrono::system_clock::now();
-    chrono::duration<double> timeCost = end - begin;
-    cout << "init_minc : " << timeCost.count() << "s." << endl;
+    evolutionOperator += tildeTMatrices[0] * cyl_bessel_j(0, z);
     return evolutionOperator;
 }
+
+//SparseMatrixXcd getEvolutionOperatorForOneTimestep() {
+//    auto begin = chrono::system_clock::now();
+//    double z = TIME_STEP * maxEntry;
+//    auto B = -H / maxEntry;
+//    SparseMatrixXcd evolutionOperator;
+//    evolutionOperator = SparseMatrixXcd::Zero(OPERATOR_SIZE, OPERATOR_SIZE);
+//    complex<double> jv = 1;
+//    vector<SparseMatrixXcd> tildeTMatrices = {SparseMatrixXcd::Identity(OPERATOR_SIZE, OPERATOR_SIZE), B * complex<double>(0., 1.)};
+//    auto properOrder = findProperApproximationOrder(z);
+//    cout << properOrder << endl;
+//    for (size_t i = 1; i <= properOrder; ++i) {
+//        jv = cyl_bessel_j(i, z);
+//        evolutionOperator += jv * tildeTMatrices[0];
+//        auto nextTildeT = B * complex<double>(0., 2.) * tildeTMatrices[1] + tildeTMatrices[0];
+//        tildeTMatrices[0] = tildeTMatrices[1];
+//        tildeTMatrices[1] = nextTildeT;
+//        cout << i << endl;
+//    }
+//    evolutionOperator *= 2.0;
+//    evolutionOperator += SparseMatrixXcd::Identity(OPERATOR_SIZE, OPERATOR_SIZE) * cyl_bessel_j(0, z);
+//    auto end = chrono::system_clock::now();
+//    chrono::duration<double> timeCost = end - begin;
+//    cout << "init_minc : " << timeCost.count() << "s." << endl;
+//    return evolutionOperator;
+//}
 
 
 
