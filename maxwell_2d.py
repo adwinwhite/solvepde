5f733ff7cd49890d1bd27c468be44e6c89d7ae2a
AdwinKing <jin2000112014@gmail.com>
2020-10-18 14:35:18 +0800

    wrote hamiltonian

--- maxwell_2d.py
+++ maxwell_2d.py
@@ -25,11 +25,16 @@ light_speed = 1
 electric_constant = 1
 magnetic_constant = 1
 mesh_step = 1
-num_of_frames = 1
+num_of_frames = 10000
 time_step = 0.1
 electric_direction = np.array((0, 1, 0))
 k = np.array((10, 0, 0))
 E_0 = 10
+allowed_error = 10**(-13)
+max_order_of_chebyshev_poly = 100000
+display_size_step = 2
+
+
 free_plane = np.array([-grid_size[0] / 2, -grid_size[1] / 2, grid_size[0] / 2, grid_size[1] /2]) * mesh_step
 free_plane_length = (grid_size[0] * mesh_step, grid_size[1] * mesh_step)
 packet_center = (0, 0)
@@ -101,26 +106,26 @@ def get_discretized_init_wave_function():
 def flatten_hamiltionian(i, j):
     rows = [np.zeros((grid_size[1] + 1, grid_size[0] + 1, 3)) for m in range(3)]
     if j + 1 <= grid_size[1]:
-        row[0][j + 1][i][2] = 1
+        rows[0][j + 1][i][2] = 1
     if j - 1 >= 0:
-        row[0][j - 1][i][2] = -1
-    row[0] /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
+        rows[0][j - 1][i][2] = -1
+    rows[0] /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
 
     if i + 1 <= grid_size[0]:
-        row[1][j][i + 1][2] = 1
+        rows[1][j][i + 1][2] = 1
     if i - 1 >= 0:
-        row[1][j][i - 1][2] = -1
-    row2 /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
+        rows[1][j][i - 1][2] = -1
+    rows[1] /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
 
     if j + 1 <= grid_size[1]:
-        row[2][j + 1][i][0] = 1
+        rows[2][j + 1][i][0] = 1
     if j - 1 >= 0:
-        row[2][j - 1][i][0] = -1
+        rows[2][j - 1][i][0] = -1
     if i + 1 <= grid_size[0]:
-        row[2][j][i + 1][1] = -1
+        rows[2][j][i + 1][1] = -1
     if i - 1 >= 0:
-        row[2][j][i - 1][1] = 1
-    row[2] /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
+        rows[2][j][i - 1][1] = 1
+    rows[2] /= electric_coeff_sqrt * magnetic_coeff_sqrt * 2 * mesh_step
 
     return sparse.vstack([sparse.csr_matrix(row.flatten()) for row in rows])
 
@@ -135,53 +140,53 @@ def get_hamiltonian():
     return sparse.vstack(hamiltonian)
 
 
-# T_tilde_matrices = [None, sparse.identity(operator_size)]
-# def next_T_tilde_matrix(B):
-#     if T_tilde_matrices[0] is None:
-#         T_tilde_matrices[0] = T_tilde_matrices[1]
-#         T_tilde_matrices[1] = B
-#         return T_tilde_matrices[1]
-#     else:
-#         next_T_tilde = B * 2 * T_tilde_matrices[1] - T_tilde_matrices[0]
-#         T_tilde_matrices[0] = T_tilde_matrices[1]
-#         T_tilde_matrices[1] = next_T_tilde
-#         return next_T_tilde
-#
-#
-# H = get_hamiltonian()
-# # using recursion formula for chebyshev polynomial. x's range is R rather than [-1, 1]
-# def get_evolution_operator_one_timestep():
-#     print(H.shape)
-#     eigen_factor = 64
-#     # print("det(H) : {}".format(scipy.linalg.det(H)))
-#     # max_eigenvalue = eigsh(H, k=1, which="LA")[0][0]
-#     # min_eigenvalue = eigsh(H, k=1, which="SA")[0][0]
-#     max_eigenvalue = 0
-#     min_eigenvalue = -8
-#     print("max_eigenvalue : {}".format(max_eigenvalue))
-#     print("min_eigenvalue : {}".format(min_eigenvalue))
-#     z = (max_eigenvalue - min_eigenvalue) * time_step / eigen_factor
-#     B = ((H - sparse.identity(operator_size) * (max_eigenvalue + min_eigenvalue) / 2) / (max_eigenvalue - min_eigenvalue)) * (-1j) * eigen_factor
-#     # print("det(B) : {}".format(scipy.linalg.det(B)))
-#     evolution_operator = sparse.csr_matrix((operator_size, operator_size), dtype=np.complex128)
-#     jv = 1
-#     i = 1
-#     while abs(jv) > allowed_error and i <= max_order_of_chebyshev_poly:
-#         jv = scipy.special.jv(i, z)
-#         # evolution_operator += jv * next_T_tilde_matrix(B)
-#         tmpT = jv * next_T_tilde_matrix(B)
-#         print(i)
-#         # if sparse.linalg.det(tmpT) == 0:
-#         #     print("{}".format(i))
-#         evolution_operator += tmpT
-#         i += 1
-#     evolution_operator = (evolution_operator * 2 + sparse.identity(operator_size, dtype=np.complex128) * scipy.special.jv(0, z)) * np.exp((max_eigenvalue + min_eigenvalue) * time_step * (-0.5j))
-#     print("{} : {}".format(i, abs(jv)))
-#     # detm = scipy.linalg.det(evolution_operator * evolution_operator.transpose().conj())
-#     # factor = pow(1 / detm, 1 / operator_size)
-#     # evolution_operator *= factor
-#     # print("{}".format(scipy.linalg.det(evolution_operator * evolution_operator.transpose().conj())))
-#     return sparse.csr_matrix(evolution_operator)
+T_tilde_matrices = [None, sparse.identity(operator_size)]
+def next_T_tilde_matrix(B):
+    if T_tilde_matrices[0] is None:
+        T_tilde_matrices[0] = T_tilde_matrices[1]
+        T_tilde_matrices[1] = B
+        return T_tilde_matrices[1]
+    else:
+        next_T_tilde = B * 2 * T_tilde_matrices[1] - T_tilde_matrices[0]
+        T_tilde_matrices[0] = T_tilde_matrices[1]
+        T_tilde_matrices[1] = next_T_tilde
+        return next_T_tilde
+
+
+H = get_hamiltonian()
+# using recursion formula for chebyshev polynomial. x's range is R rather than [-1, 1]
+def get_evolution_operator_one_timestep():
+    print(H.shape)
+    eigen_factor = 64
+    # print("det(H) : {}".format(scipy.linalg.det(H)))
+    # max_eigenvalue = eigsh(H, k=1, which="LA")[0][0]
+    # min_eigenvalue = eigsh(H, k=1, which="SA")[0][0]
+    max_eigenvalue = 0
+    min_eigenvalue = -8
+    print("max_eigenvalue : {}".format(max_eigenvalue))
+    print("min_eigenvalue : {}".format(min_eigenvalue))
+    z = (max_eigenvalue - min_eigenvalue) * time_step / eigen_factor
+    B = ((H - sparse.identity(operator_size) * (max_eigenvalue + min_eigenvalue) / 2) / (max_eigenvalue - min_eigenvalue)) * (-1j) * eigen_factor
+    # print("det(B) : {}".format(scipy.linalg.det(B)))
+    evolution_operator = sparse.csr_matrix((operator_size, operator_size), dtype=np.complex128)
+    jv = 1
+    i = 1
+    while abs(jv) > allowed_error and i <= max_order_of_chebyshev_poly:
+        jv = scipy.special.jv(i, z)
+        # evolution_operator += jv * next_T_tilde_matrix(B)
+        tmpT = jv * next_T_tilde_matrix(B) * (1j)**i
+        print(i)
+        # if sparse.linalg.det(tmpT) == 0:
+        #     print("{}".format(i))
+        evolution_operator += tmpT
+        i += 1
+    evolution_operator = (evolution_operator * 2 + sparse.identity(operator_size, dtype=np.complex128) * scipy.special.jv(0, z)) * np.exp((max_eigenvalue + min_eigenvalue) * time_step * 0.5)
+    print("{} : {}".format(i, abs(jv)))
+    # detm = scipy.linalg.det(evolution_operator * evolution_operator.transpose().conj())
+    # factor = pow(1 / detm, 1 / operator_size)
+    # evolution_operator *= factor
+    # print("{}".format(scipy.linalg.det(evolution_operator * evolution_operator.transpose().conj())))
+    return sparse.csr_matrix(evolution_operator)
 #
 #
 # def fake_border(wave):
@@ -228,21 +233,27 @@ def get_hamiltonian():
 #     return wave
 #
 #
-# evolution_operator = get_evolution_operator_one_timestep()
-# current_wave = normalize_wave(get_discretized_init_wave_function())
-# def propagate_wave(steps=1):
-#     global current_wave
-#     # current_wave = evolution_operator.dot(fake_border(current_wave))
-#     for i in range(steps):
-#         # current_wave = normalize_wave(evolution_operator.dot(fake_border(current_wave)))
-#     # current_wave = normalize_wave(apply_damping(evolution_operator.dot(current_wave), damping_factor=0.9, border_size=6))
-#         current_wave = normalize_wave(evolution_operator.dot(current_wave))
-#     return current_wave
+evolution_operator = get_evolution_operator_one_timestep()
+current_wave = get_discretized_init_wave_function()
+def propagate_wave(steps=1):
+    global current_wave
+    # current_wave = evolution_operator.dot(fake_border(current_wave))
+    for i in range(steps):
+        # current_wave = normalize_wave(evolution_operator.dot(fake_border(current_wave)))
+    # current_wave = normalize_wave(apply_damping(evolution_operator.dot(current_wave), damping_factor=0.9, border_size=6))
+        current_wave = evolution_operator.dot(current_wave)
+    return current_wave
+
+def wave2energe(wave):
+    unflattened = np.reshape(wave, (grid_size[1] + 1, grid_size[0] + 1, 3))[::display_size_step, ::display_size_step, ::]
+    # print(unflattened.shape)
+    dis = np.array([[np.linalg.norm(v)**2 / 2 for v in unflattened[j]] for j in range(int(grid_size[1] / display_size_step) + 1)])
+    return dis
 
 
 
-xs = np.linspace(free_plane[0], free_plane[2], grid_size[0] + 1)
-ys = np.linspace(free_plane[1], free_plane[3], grid_size[1] + 1)
+xs = np.linspace(free_plane[0], free_plane[2], int(grid_size[0] / display_size_step) + 1)
+ys = np.linspace(free_plane[1], free_plane[3], int(grid_size[1] / display_size_step) + 1)
 xs, ys = np.meshgrid(xs, ys)
 # ps = np.array([[get_potential(i * mesh_step, j * mesh_step) / 1000 for i in range(grid_size[0])] for j in range(grid_size[1])])
 
@@ -255,8 +266,9 @@ def update_plot(frame_number):
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.invert_xaxis()
-    es = energy_density(frame_number * time_step)
-    ax.plot_surface(xs, ys, es, cmap="coolwarm")
+    propagate_wave(steps=1)
+    dis = wave2energe(current_wave)
+    ax.plot_surface(xs, ys, dis, cmap="coolwarm")
     print(frame_number)
 
 # Writer = animation.writers['ffmpeg']
