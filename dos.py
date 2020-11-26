79fab25e56c5e98f3a48463596742dab5afaa565
AdwinKing <jin2000112014@gmail.com>
2020-11-27 00:16:05 +0800

    .

--- dos.py
+++ dos.py
@@ -3,34 +3,31 @@ import scipy.special
 import cmath
 import math
 import copy
+import multiprocessing as mp
 
 
 import matplotlib
-# matplotlib.use("Agg")
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d.axes3d import Axes3D
-from mpl_toolkits.mplot3d import proj3d
-from matplotlib.animation import FuncAnimation
 from scipy.sparse.linalg import eigsh
 from scipy import sparse
+from scipy.signal import windows
 # from sparse_dot_mkl import dot_product_mkl
 
 
-
+# grid size, timestep and sample size are irrelevant to the shape of the output figure.
 
 
 grid_size = (128, 128)
-time_step = 0.01
+time_step = 0.001
 max_order_of_chebyshev_poly = 100000
 allowed_error = 10**(-13)
 operator_size = grid_size[0] * grid_size[1]
-t_n = -2.7
-t_nn = -0.4
-on_site_potential = 2.7
+t_n = -2.77
+t_nn = -0.1
+on_site_potential = -0.3
 
-sample_size = 512
+sample_size = 1024
 num_of_evolution_steps = 1
 dt = time_step * num_of_evolution_steps
 
@@ -84,6 +81,8 @@ def flatten_hamiltionian(i, j):
 
 
 def get_hamiltonian():
+    # pool = mp.Pool(mp.cpu_count())
+    # hamiltonian = [pool.apply(flatten_hamiltionian, args=(i, j)) for j in range(grid_size[1]) for i in range(grid_size[0])]
     hamiltonian = []
     for j in range(grid_size[1]):
         for i in range(grid_size[0]):
@@ -107,17 +106,17 @@ def next_T_tilde_matrix(B):
 
 H = get_hamiltonian()
 # using recursion formula for chebyshev polynomial. x's range is R rather than [-1, 1]
-def get_evolution_operator_one_timestep():
+def get_evolution_operator_one_timestep(dt):
     print(H.shape)
-    eigen_factor = 64
+    eigen_factor = 2
     # print("det(H) : {}".format(scipy.linalg.det(H)))
-    max_eigenvalue = eigsh(H, k=1, which="LA")[0][0]
-    min_eigenvalue = eigsh(H, k=1, which="SA")[0][0]
-    # max_eigenvalue = 0
-    # min_eigenvalue = -8
+    # max_eigenvalue = eigsh(H, k=1, which="LA")[0][0]
+    # min_eigenvalue = eigsh(H, k=1, which="SA")[0][0]
+    max_eigenvalue = 10
+    min_eigenvalue = -10
     print("max_eigenvalue : {}".format(max_eigenvalue))
     print("min_eigenvalue : {}".format(min_eigenvalue))
-    z = (max_eigenvalue - min_eigenvalue) * time_step / eigen_factor
+    z = (max_eigenvalue - min_eigenvalue) * dt / eigen_factor
     B = ((H - sparse.identity(operator_size) * (max_eigenvalue + min_eigenvalue) / 2) / (max_eigenvalue - min_eigenvalue)) * (-1j) * eigen_factor
     # print("det(B) : {}".format(scipy.linalg.det(B)))
     evolution_operator = sparse.csr_matrix((operator_size, operator_size), dtype=np.complex128)
@@ -132,7 +131,7 @@ def get_evolution_operator_one_timestep():
         #     print("{}".format(i))
         evolution_operator += tmpT
         i += 1
-    evolution_operator = (evolution_operator * 2 + sparse.identity(operator_size, dtype=np.complex128) * scipy.special.jv(0, z)) * np.exp((max_eigenvalue + min_eigenvalue) * time_step * (-0.5j))
+    evolution_operator = (evolution_operator * 2 + sparse.identity(operator_size, dtype=np.complex128) * scipy.special.jv(0, z)) * np.exp((max_eigenvalue + min_eigenvalue) * dt * (-0.5j))
     print("{} : {}".format(i, abs(jv)))
     # detm = scipy.linalg.det(evolution_operator * evolution_operator.transpose().conj())
     # factor = pow(1 / detm, 1 / operator_size)
@@ -148,29 +147,40 @@ def normalize_state(state):
 
 
 
-evolution_operator = get_evolution_operator_one_timestep()
+evolution_operator = get_evolution_operator_one_timestep(time_step)
+# inverse_evolution_operator = get_evolution_operator_one_timestep(-time_step)
+# print(sparse.linalg.norm(evolution_operator.multiply(inverse_evolution_operator)))
 initial_state = get_initial_state()
-current_state = copy.deepcopy(initial_state)
-def propagate_wave(steps=1):
-    global current_state
+def evolve(state, steps=1, backwards=False):
+    tmp_state = state
+    # if backwards:
+    #     for i in range(steps):
+    #         tmp_state = normalize_state(inverse_evolution_operator.dot(tmp_state))
+    # else:
     for i in range(steps):
-        current_state = normalize_state(evolution_operator.dot(current_state))
-    return current_state
+        tmp_state = normalize_state(evolution_operator.dot(tmp_state))
+    return tmp_state
 
 
 def get_expectation_samples():
     samples = [None] * sample_size
+    current_state = copy.deepcopy(initial_state)
     for i in range(sample_size):
-        current_state = propagate_wave(steps=num_of_evolution_steps)
+        current_state = evolve(current_state, steps=num_of_evolution_steps)
         samples[i] = initial_state.dot(current_state)
         print(i)
     return samples
 
-def get_energy_distribution():
+def get_dos():
     samples = get_expectation_samples()
-    g = np.fft.fft(samples)
+    gaussian_window = windows.gaussian(sample_size, sample_size * 0.1)
+    samples *= gaussian_window
+    g = np.fft.fft(samples) * dt
     w = np.fft.fftfreq(sample_size)*2*np.pi/dt
-    plt.scatter(w, np.abs(g))
+    plt.scatter(w, np.real(g))
+    plt.scatter(w, np.imag(g))
+
+    plt.gca().set_xlim(-100, 100)
     plt.show()
 
-get_energy_distribution()
+get_dos()
